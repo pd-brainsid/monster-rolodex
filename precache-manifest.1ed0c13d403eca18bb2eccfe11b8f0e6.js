98619bda97a64c37276dbeb11a64f505dd25afc0
brainsid <pd-brainsid@alterpost.org>
2020-03-18 15:09:58 +0200

    Updates

---
+++ precache-manifest.1ed0c13d403eca18bb2eccfe11b8f0e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8bae727ad6669d16d61cc81c299264",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6a706d916078081a9626",
+    "url": "/monster-rolodex/static/css/main.d254ee0a.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monster-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a706d916078081a9626",
+    "url": "/monster-rolodex/static/js/main.c89448b6.chunk.js"
+  },
+  {
+    "revision": "82aa35cf669de029c632",
+    "url": "/monster-rolodex/static/js/runtime-main.0d2831a3.js"
+  }
+]);
